b9ad36aba9a5c77c7e47fb28948bc04a074aa9d4
Francisco Javier Trujillo Mata <fjtrujy@gmail.com>
2023-06-18 19:36:42 +0200

    Upgrading to newlib 4.3.0

--- scripts/003-newlib.sh
+++ scripts/003-newlib.sh
@@ -4,7 +4,7 @@
 ## Download the source code.
 REPO_URL="https://github.com/pspdev/newlib.git"
 REPO_FOLDER="newlib"
-BRANCH_NAME="allegrex-v4.1.0"
+BRANCH_NAME="allegrex-v4.3.0"
 if test ! -d "$REPO_FOLDER"; then
 	git clone --depth 1 -b $BRANCH_NAME $REPO_URL && cd $REPO_FOLDER || { exit 1; }
 else
