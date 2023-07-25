685ac7653b89a000b2844935fc470ad228f35b80
Wally4000 <catapultit@me.com>
2023-07-25 10:24:04 +1000

    Dependancies are checked in pspdev repo

--- prepare-debian-ubuntu.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-# Install build dependencies
-sudo apt-get install $@ \
-  file bison make flex texinfo gettext g++ gcc git libgmp3-dev libmpfr-dev libmpc-dev libncurses5-dev
--- prepare-fedora.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-# Install build dependencies
-sudo dnf install $@ \
-  gcc gcc-c++ make git gettext texinfo bison flex gmp-devel mpfr-devel libmpc-devel \
-  ncurses-devel diffutils
