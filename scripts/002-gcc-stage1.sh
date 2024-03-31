1de99f4cb26062223a5f49e983bb439b8cb39067
Francisco Javier Trujillo Mata <fjtrujy@gmail.com>
2024-03-31 14:09:37 +0200

    Update flags for windows compilation
    Update mysys2 packages
    
    fix macos compilation
    
    Remove specific workaround for windows

--- scripts/002-gcc-stage1.sh
+++ scripts/002-gcc-stage1.sh
@@ -33,14 +33,6 @@ fi
 cd "$REPO_FOLDER"
 
 TARGET="psp"
-OSVER=$(uname)
-
-## Apple needs to pretend to be linux
-if [ ${OSVER:0:6} == Darwin ]; then
-	TARG_XTRA_OPTS="--build=i386-linux-gnu --host=i386-linux-gnu"
-else
-	TARG_XTRA_OPTS=""
-fi
 
 ## Determine the maximum number of processes that Make can work with.
 PROC_NR=$(getconf _NPROCESSORS_ONLN)
