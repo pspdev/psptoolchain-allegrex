fa7cc8b02466c9fa0bde1f8bda1179b71838ef7c
Diamond Rivero <diamant3@proton.me>
2024-12-17 13:17:18 +0800

    set value to c++14

--- scripts/005-gcc-stage2.sh
+++ scripts/005-gcc-stage2.sh
@@ -70,7 +70,7 @@ cd build-$TARGET-stage2
   --enable-threads=posix \
   --disable-tls \
   --disable-nls \
-  --enable-cxx-flags="-fno-enforce-eh-specs" \
+  --enable-cxx-flags="c++14" \
   $TARG_XTRA_OPTS
 
 ## Compile and install.
