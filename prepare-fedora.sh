4d34d459b53b47956ab2ff32f8348a9c895caeef
Wouter Wijsman <wwijsman@live.nl>
2025-04-18 00:40:48 +0200

    Improve prepare-fedora.sh

--- prepare-fedora.sh
+++ prepare-fedora.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
+if [ "$EUID" != 0 ]; then
+    echo "Elevating to root so packages can be installed"
+    sudo "$0"
+    exit $?
+fi
 
 # Install build dependencies
-sudo dnf install $@ \
-  gcc gcc-c++ make git gettext texinfo bison flex gmp-devel mpfr-devel libmpc-devel \
-  ncurses-devel diffutils gawk file readline-devel
+dnf -y install gcc gcc-c++ make git gettext texinfo bison flex gmp-devel mpfr-devel libmpc-devel \
+    ncurses-devel diffutils glibc-gconv-extra which gawk file readline-devel
