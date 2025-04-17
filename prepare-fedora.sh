a53ce4ea7caf1f05532e9f2dd5ba997de8c24764
Wouter Wijsman <wwijsman@live.nl>
2025-04-18 00:38:06 +0200

    Add missing dependencies to prepare-fedora.sh

--- prepare-fedora.sh
+++ prepare-fedora.sh
@@ -3,4 +3,4 @@
 # Install build dependencies
 sudo dnf install $@ \
   gcc gcc-c++ make git gettext texinfo bison flex gmp-devel mpfr-devel libmpc-devel \
-  ncurses-devel diffutils
+  ncurses-devel diffutils gawk file readline-devel
