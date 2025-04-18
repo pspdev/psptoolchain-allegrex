c057fc74547f4eb27bef1db910d438831b9d16f2
Wouter Wijsman <wwijsman@live.nl>
2025-04-18 14:57:26 +0200

    Make sure readline dev libraries are available for all platforms

--- prepare-debian-ubuntu.sh
+++ prepare-debian-ubuntu.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
+if [ "$EUID" != 0 ]; then
+    echo "Elevating to root so packages can be installed"
+    sudo "$0"
+    exit $?
+fi
 
 # Install build dependencies
-sudo apt-get install $@ \
-  file bison make flex texinfo gettext g++ gcc git libgmp3-dev libmpfr-dev libmpc-dev libncurses5-dev
+apt-get update
+apt-get install -y file bison make flex texinfo gettext g++ gcc git libgmp3-dev libmpfr-dev libmpc-dev libncurses5-dev libreadline-dev
